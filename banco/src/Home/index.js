b516bae95e0e33f3e1b3e2a2bda42e9f5070c157
Arley Souto <macx15@hotmail.com>
2021-11-05 00:54:01 -0300

    Feito a mascar e upload de arquivos
    Foi feito a mascara de cpf e feito o upload de arquivos

--- banco/src/Home/index.js
+++ banco/src/Home/index.js
@@ -1,45 +1,76 @@
-import React from "react";
+import { useState } from "react";
 import './home.css';
+import { cpfMask } from "../components/CPFMask/cpfmask";
+
 
 export default function Home() {
+    const [file, setFile] = useState([]);
+    const [cpf, setCpf] = useState();
 
+    function handleCpf(e) {
+        setCpf(cpfMask( e.target.value ))
+    }
 
+    const handleUploadFile = (e) => {
+        //setFile(files);
+        var reader = new FileReader();
+        const files = e.target.files[0];
+        reader.readAsText(files)
+        reader.onload = function (event) {
+            let fileContent = event.target.result;
+            const allLines = fileContent.split(/\r \n | \n/);
+            //setFile(allLines)
+            console.log(files)
+
+            allLines.forEach((line, indx) => {
+                console.log(`current-line ${line} line-index ${indx}`)
+                setFile({
+                    texto: line,
+                    index: indx,
+                    info: files.name
+                })
+            })
+        }
+
+    }
 
     return (
         <div className="containerHome">
+            <h1 className="title">Documentação do CNAB</h1>
             <div className="formOrg">
                 <form className="form">
                     <label className="labelInput">Tipo de Transação</label>
-                    <input className="input" placeholder="Tipo de Transação" autoFocus />
+                    <input type="text" className="input" placeholder="Tipo de Transação" autoFocus />
 
                     <label className="labelInput">Data da Ocorrencia</label>
-                    <input className="input" placeholder="Tipo de Transação" />
+                    <input type="date" className="input" placeholder="Tipo de Transação" />
 
                     <label className="labelInput">Valor da movimentação</label>
-                    <input className="input" placeholder="Tipo de Transação" />
+                    <input type="number" className="input" placeholder="Tipo de Transação" />
 
                     <label className="labelInput">CPF do beneficiario</label>
-                    <input className="input" placeholder="Tipo de Transação" />
+                    <input name="cpf" value={cpf} onChange={handleCpf} className="input" placeholder="Tipo de Transação" maxLength="14" />
 
                     <label className="labelInput">Cartão utilizado na transação</label>
-                    <input className="input" placeholder="Tipo de Transação" />
+                    <input type="number" className="input" placeholder="Tipo de Transação" />
 
                     <label className="labelInput">Hora da ocorrência</label>
-                    <input className="input" placeholder="Tipo de Transação" />
+                    <input type="time" className="input" placeholder="Tipo de Transação" />
 
                     <label className="labelInput">Nome do representando da loja</label>
-                    <input className="input" placeholder="Tipo de Transação" />
+                    <input type="text" className="input" placeholder="Tipo de Transação" />
 
                     <label className="labelInput">Nome da loja</label>
-                    <input className="input" placeholder="Tipo de Transação" />
+                    <input type="text" className="input" placeholder="Tipo de Transação" />
 
-                    <div className="upload">
-                        <label for="labelInput">arquivo CNAB</label>
-                        <input id="labelInput" type="file" accept="application/pdf" />
+                    <div className='uploadContainer'>
+                        <label className="upload" for="labelInput">Arquivo CNAB</label>
+                        <input onChange={handleUploadFile} id="labelInput" type="file" accept="application/txt" />
+                        {file.info}
                     </div>
-
                 </form>
             </div>
+
         </div>
     )
 }
